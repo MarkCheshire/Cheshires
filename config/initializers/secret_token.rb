19f1ab1111fa8042e21cd9cc650cdc06a132528a
Mark Cheshire <Mark_Cheshire@alum.mit.edu>
2012-08-21 19:22:15 +0200

    First Cheshire home page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cheshires::Application.config.secret_token = '3ae90b13e2a8f0b7134ea925a24503c88dd2a5aac6e4bb227c52c22bb24bab776645fbf8179870adb5619c41edb0ba17bec7fb8ec583a1ccc888d77db50a051a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Cheshires::Application.config.session_store :cookie_store, :key => '_cheshires_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Cheshires::Application.config.session_store :active_record_store
